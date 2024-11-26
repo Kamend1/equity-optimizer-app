2f4cd2c34aece59581538916cc5ee8666143fc5a
kam.admin <kam.admin@admla0058.arexim.bg>
2024-11-26 14:11:55 +0200

    Add: automatic scheduling with celery and Redis

--- EquityOptimizerApp/common/admin.py
+++ EquityOptimizerApp/common/admin.py
@@ -1,22 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from django_celery_beat.models import PeriodicTask, IntervalSchedule, CrontabSchedule, SolarSchedule, ClockedSchedule
 from django_celery_results.models import TaskResult
 
-# Register Celery Beat models
-admin.site.register(IntervalSchedule)
-admin.site.register(CrontabSchedule)
-admin.site.register(SolarSchedule)
-admin.site.register(ClockedSchedule)
-
-
+# Register only TaskResult and customize its admin interface
 @admin.register(TaskResult)
 class TaskResultAdmin(admin.ModelAdmin):
     list_display = ('task_name', 'status', 'date_done', 'worker', 'result',)
     search_fields = ('task_name', 'worker',)
     list_filter = ('status', 'date_done',)
+
+# Optionally customize the PeriodicTask admin interface
+from django_celery_beat.models import PeriodicTask
 
 @admin.register(PeriodicTask)
 class PeriodicTaskAdmin(admin.ModelAdmin):
